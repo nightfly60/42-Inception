aacf7f289fd8bbb94230bbac30550f42fca60a23
elena <elena@virtualmachine.myguest.virtualbox.org>
2026-01-07 14:19:56 +0100

    wordpress & maria connexion

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -1,37 +1,36 @@
 #!/bin/sh
+set -e
 
 # si la db existe pas on va la créer
 if [ ! -d "/var/lib/mysql/mysql" ]; then
-    echo "=== Première installation ==="
-    echo "Initialisation de mariadb..."
 
     mysql_install_db --user=mysql --datadir=/var/lib/mysql
 
-    echo "Structure de base créée."
-    echo "Configuration des utilisateurs et de la base de données..."
+    mysqld_safe --skip-networking &
+    pid="$!"
+    sleep 5
 
-    /usr/bin/mysqld --user=mysql --bootstrap << EOF
-USE mysql;
-FLUSH PRIVILEGES;
+    mysql -u root <<EOF
 
 -- création d'un password pour le root
 ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
 
 -- création de la database
-CREATE DATABASE IF NOT EXISTS \'${MYSQL_DATABASE}\';
+CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
 
 -- création du user wordpress
 CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
 
 -- on donne tous les accès au user wordpress sur la database
-GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
+GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
+
+FLUSH PRIVILEGES;
 
 EOF
 
-    echo "=== Config terminée ==="
-    echo "Base de données : ${MYSQL_DATABASE}"
-    echo "User : ${MYSQL_USER}"
+    mysqladmin -u root -p"${MYSQL_ROOT_PASSWORD}" shutdown
+    wait "$pid"
+
 fi
 
-echo "Démarrage de mariadb..."
-exec /usr/bin/mysqld --user=mysql --console
+exec mysqld_safe --user=mysql --console
